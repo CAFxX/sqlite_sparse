328b2afb041e71b9f5ecab6723f12e6cf342089a
Carlo Alberto Ferraris <cafxx@strayorange.com>
2019-03-19 22:39:34 +0900

    Update sqlite_sparse.c

--- sqlite_sparse.c
+++ sqlite_sparse.c
@@ -57,6 +57,13 @@ static ssize_t pread(int fd, void *buf, size_t count, off_t offset) {
 #define O_BINARY 0
 #endif
 
+static int32_t numpages(int fd, size_t pagesize) {
+    off_t off = lseek(fd, 0, SEEK_END);
+    assert(off != -1);
+    assert(off % pagesize == 0);
+    return off/pagesize;
+}   
+
 static int32_t readpageindex(int fd, size_t off) {
     uint32_t v;
     assert(sizeof(v) == 4);
@@ -94,23 +101,35 @@ int main(int argc, char **argv) {
     checkheader(fd);
 
     size_t pagesize = readpagesize(fd);
+    int32_t pages = getnumpages(fd, pagesize);
+    char *bitmap = calloc((pages+7)/8+1, 1);
     int32_t freelistpage = readpageindex(fd, 32);
     size_t freed = 0;
+
+    while (freelistpage > 1) {
+        size_t pageoff = (freelistpage-1)*pagesize;
+        int32_t L = readpageindex(fd, pageoff+4);
+        assert((L+2)*4 < pagesize);
+        for (int i = 0; i < L; i++) {
+            int32_t freepage = readpageindex(fd, pageoff+(i+2)*4) - 1;
+            assert(freepage > 0);
+            assert(freepage < pages);
+            bitmap[freepage/8] |= 1<<(freepage%8);
+            freed++;
+        }
+        freelistpage = readpageindex(fd, pageoff+0);
+    }
 
 #ifdef __windows__
     HANDLE h = (HANDLE)_get_osfhandle(fd);
     int sparse = 0;
 #endif
 
-    while (freelistpage > 1) {
-        size_t pageoff = (freelistpage-1)*pagesize;
-        int32_t L = readpageindex(fd, pageoff+4);
-        // printf("freelistpage %d, L %d\n", freelistpage, L);
-        assert((L+2)*4 < pagesize);
-        for (int i = 0; i < L; i++) {
-            int32_t freepage = readpageindex(fd, pageoff+(i+2)*4);
-            assert(freepage > 1);
-            // printf("Deallocating page %d\n", freepage);
+    int32_t first = -1;
+    for (i = 0; i < pages+1; i++) {
+        if (first == -1 && (bitmap[i/8]&(1<<(i%8)) == 1)) {
+            first = i;
+        } else if (first != -1 && (bitmap[i/8]&(1<<(i%8)) == 0)) {
 #if defined(__windows__)
             DWORD unused;
             if (!sparse) {
@@ -118,15 +137,14 @@ int main(int argc, char **argv) {
                 sparse = 1;
             }
             FILE_ZERO_DATA_INFORMATION fzdi;
-            fzdi.FileOffset.QuadPart = pagesize*(freepage-1);
-            fzdi.BeyondFinalZero.QuadPart = pagesize*freepage;
+            fzdi.FileOffset.QuadPart = pagesize*first;
+            fzdi.BeyondFinalZero.QuadPart = pagesize*i;
             assert(DeviceIoControl(h, FSCTL_SET_ZERO_DATA, &fzdi, sizeof(fzdi), NULL, 0, &unused, NULL));
 #elif defined(__linux__)
-            assert(fallocate(fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE, pagesize*(freepage-1), pagesize) == 0);
-#endif
-            freed++;
+            assert(fallocate(fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE, pagesize*first, (i-first)*pagesize) == 0);
+#endif            
+            first = -1;
         }
-        freelistpage = readpageindex(fd, pageoff+0);
     }
     
     printf("Deallocated %d pages (%d bytes)\n", freed, freed*pagesize);
